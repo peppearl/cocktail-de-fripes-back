7cd3464aa59c6c6f6247ce6cfb902e7d5d162d65
Gabrielle Geroudet <gabrielle.geroudet@gmail.com>
2021-04-12 22:48:50 +0200

    Created the blog API

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const newsRouter = require('./routers/news');
+
+app.use(express.static('public'));
+app.use(express.static('data/uploads'));
+app.use('/api', newsRouter);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}.`);
+});
+
+
+// ~/Documents/my_digital_school/bachelor_dev/cocktail_de_fripes/app/back-cdf
